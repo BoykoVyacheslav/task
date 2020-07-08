1dc2354e4f1cb22463e0f4335d868383f71d39a9
Boiko <v.boiko@finik.pro>
2020-07-08 09:43:49 +0300

    Update according to review

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { NavigationBar } from "./components/NavigationBar";
+import menu from "./resources/menu.json";
 
 function App() {
   return (
     <div className="App">
-        <NavigationBar header={ <h3>Some header</h3> }/>
+        <NavigationBar header={ <h3>Some header</h3> } menu={ menu.data }/>
     </div>
   );
 }
